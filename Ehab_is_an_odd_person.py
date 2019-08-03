7e71888538a4bc1af092972cfe4cd62f8c249fb9
jerry <jerryhuang992@gmail.com>
2019-08-03 23:19:00 +1000

    Quiz_5 optimal version(recursion version)

---
+++ Ehab_is_an_odd_person.py
@@ -0,0 +1,30 @@
+"""
+Input n, n digital read,
+for all the odd number obtained by adding the exchange position,
+the output processing sequence lexicographically smallest
+
+TRICK:
+If this list contains some odd elements and some even elements,
+which means all the elements can be any positions.
+Therefore, we just need to sort the list.
+
+If this list only has odd or even, we output the original list.
+
+"""
+
+def find(l):
+    odd = []
+    even = []
+    for i in l:
+        if i%2 == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+    if even == [] or odd == []:
+        return l
+    else:
+        return sorted(l)
+
+raw = input('Please input a list: ').split()
+L = [int(i) for i in raw]
+print('The output list: \n', find(L))
